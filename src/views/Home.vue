41f5ac6df3672eccd24809889ddbaf051bb5cf8b
Hykilpikonna <me@hydev.org>
2021-11-10 17:28:46 -0500

    [-] Remove hello world component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        Hello World
     </div>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Options({
-    components: {
-        HelloWorld,
-    },
-})
+@Options({components: {}})
 export default class Home extends Vue
 {
 }
