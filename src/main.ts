9664980857e9986b91b8d0234a84baf8de66657e
Hykilpikonna <me@hydev.org>
2021-11-11 17:36:10 -0500

    [+] Keep ElementUI for font icons

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'element-plus/dist/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import ElementPlus from 'element-plus'
 
