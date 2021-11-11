0022545d90a50e10d81b1933d01be8f757aa6e95
Hykilpikonna <me@hydev.org>
2021-11-11 15:45:15 -0500

    [+] Import element-icon and fa using modules

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App).use(router).mount('#app')
