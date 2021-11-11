f5214021003a5f7d738e6f8ecd7c305704ee154f
Hykilpikonna <me@hydev.org>
2021-11-11 10:54:31 -0500

    [+] Profile page: Simulate getting data

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,17 +1,25 @@
 <template>
     <div id="Profile">
-        {{name}}
+        {{p.name}}
     </div>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
 import {Prop} from "vue-property-decorator";
+import {exampleData, Person} from "@/logic/data";
 
 @Options({components: {}})
 export default class Profile extends Vue
 {
     @Prop() name!: string
+
+    p?: Person
+    created()
+    {
+        // TODO: Get data from server
+        this.p = exampleData.filter(it => it.name == this.name)[0]
+    }
 }
 </script>
 
