c258630fd0724dcff99a4377d70d8dda84507116
Hykilpikonna <me@hydev.org>
2021-11-11 14:50:58 -0500

    [+] Extend data model to include websites

--- src/logic/data.ts
+++ src/logic/data.ts
@@ -6,6 +6,7 @@ export interface Person
     profileUrl: string
 
     info?: {[id: string]: string}
+    websites?: {[id: string]: string}
 
     profilePageUrl?: string
     profilePageFormat?: string
@@ -23,7 +24,9 @@ export const exampleData: Person[] = [
         profileUrl: 'https://pbs.twimg.com/profile_images/591631266937638913/AtOAlQpd_400x400.jpg',
 
         info: {本名: '沟口辽', 别名: 'ぽわぽわP, 仁王立ち', 国籍: '日本', 出生: '1995年3月9日',
-            逝世: '2015年7月23日（20岁）', 职业: 'V家P主，插画家', 活跃年代: '2009年6月—2015年7月'}
+            逝世: '2015年7月23日（20岁）', 职业: 'V家P主，插画家', 活跃年代: '2009年6月—2015年7月'},
+
+        websites: {}
     },
     {
         id: 'donotexist_A',
