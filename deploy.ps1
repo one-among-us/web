2bf6fc83e253ec5ffb7e3b68b7b61b3ff6a1d29e
Hykilpikonna <me@hydev.org>
2021-11-12 17:10:20 -0500

    [+] Create powershell deploy script

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+# Build
+yarn run build
+
+# Navigate into the build output directory
+cd dist
+
+# Deploying to a custom domain
+echo 'nightly.one-among.us' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/hykilpikonna/one-among-us.git master:gh-pages
+cd -
