b040d5fcbcaaf45b23fd593aecb10412bf027916
Hykilpikonna <me@hydev.org>
2021-11-12 14:28:52 -0500

    [-] Remove example data

--- src/logic/data.ts
+++ src/logic/data.ts
@@ -1,3 +1,10 @@
+export interface PersonMeta
+{
+    path: string,
+    id: string,
+    name: string
+    profileUrl: string
+}
 
 export interface Person
 {
@@ -11,32 +18,3 @@ export interface Person
     profilePageUrl?: string
     profilePageFormat?: string
 }
-
-export const exampleData: Person[] = [
-    {
-        id: 'hykilpikonna',
-        name: '小桂桂',
-        profileUrl: 'https://pbs.twimg.com/profile_images/1445198854429810690/TzeMf5yX_400x400.jpg'
-    },
-    {
-        id: 'siinamota',
-        name: '椎名もた',
-        profileUrl: 'https://pbs.twimg.com/profile_images/591631266937638913/AtOAlQpd_400x400.jpg',
-
-        info: {本名: '沟口辽', 别名: 'ぽわぽわP, 仁王立ち', 国籍: '日本', 出生: '1995年3月9日',
-            逝世: '2015年7月23日（20岁）', 职业: 'V家P主，插画家', 活跃年代: '2009年6月—2015年7月'},
-
-        websites: {twitter: 'https://twitter.com/siinamota',
-            personal: 'http://www.siinamota.com/index.html'}
-    },
-    {
-        id: 'donotexist_A',
-        name: '不存在',
-        profileUrl: 'https://pbs.twimg.com/profile_images/1374397593594122242/bPfn-Zzk_400x400.jpg'
-    },
-    {
-        id: 'Uekawakuyuurei',
-        name: '萤',
-        profileUrl: 'https://pbs.twimg.com/profile_images/1378912446782394368/icyGMaK5_400x400.jpg'
-    },
-]
