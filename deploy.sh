accf9f2c6dde1ad891705f32d93ad4b0255607d7
Hykilpikonna <me@hydev.org>
2021-11-12 16:23:04 -0500

    [+] Create deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Abort on errors
+set -e
+
+# Build
+yarn run build
+
+# Navigate into the build output directory
+cd dist
+
+# Deploying to a custom domain
+echo 'nightly.one-among.us' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:hykilpikonna/one-among-us.git master:gh-pages
+cd -
