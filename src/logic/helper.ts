deea9a6533cf9ed0132a9a5580b040666e736dac
Hykilpikonna <me@hydev.org>
2021-11-13 17:29:59 -0500

    [+] Some helper functions

--- src/logic/helper.ts
+++ src/logic/helper.ts
@@ -65,3 +65,13 @@ export function getTodayDate(): string
 {
     return moment().format('YYYY-MM-DD')
 }
+
+export function randint(min: number, max: number): number
+{
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export function rand(min: number, max: number): number
+{
+    return Math.random() * (max - min + 1) + min
+}
