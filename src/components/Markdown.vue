349a6e69fe3cf484125720c4337f429d444f5f75
Hykilpikonna <me@hydev.org>
2021-11-19 18:47:15 -0500

    [S] Justify markdown

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -21,6 +21,34 @@ export default class Markdown extends Vue
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
+@import "../css/colors"
 
+// Markdown style
+.markdown-content
+    text-align: justify
+    text-justify: inter-word
+    margin: 10px min(4vw, 40px)
+
+    a
+        color: $color-text-special
+        text-decoration: none
+
+    h1, h2
+        border-bottom: 1px solid $color-text-special
+        font-size: 1.5em
+        margin-top: 1em
+
+    h1, h2
+        line-height: 1.3
+        margin-bottom: 0.25em
+        padding: 0
+
+    p
+        font-size: 0.875em
+        margin: 0.5em 0
+        line-height: 1.6
+
+    li
+        font-size: 0.875em
 </style>
