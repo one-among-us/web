e78e1229907e4a7083143b84808afd067e510301
Mak1o <96227977+Mak1o@users.noreply.github.com>
2022-01-13 00:08:18 +0800

    [O] Use normal web history instead of hash history

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,7 @@ yarn run build
 
 # Navigate into the build output directory
 cd dist
+cp index.html 404.html
 
 # Deploying to a custom domain
 echo 'www.one-among.us' > CNAME
--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ yarn run build
 
 # Navigate into the build output directory
 cd dist
+cp index.html 404.html
 
 # Deploying to a custom domain
 echo 'www.one-among.us' > CNAME
