4fde39fa2be87af1e18af305575b3c9e0af63170
Hykilpikonna <me@hydev.org>
2022-06-24 09:10:06 -0400

    [+] Custom markdown spoiler parser

--- src/logic/ui.ts
+++ src/logic/ui.ts
@@ -1,3 +1,7 @@
+/**
+ * Initialize clickable spoilers on the page
+ */
+import {marked} from "marked";
 
 export function initSpoilers()
 {
@@ -21,3 +25,37 @@ export function initSpoilers()
     })
   }
 }
+
+/**
+ * Spoiler markdown extension
+ */
+const spoilerExtension = {
+  name: 'spoiler',
+  level: 'inline',
+  start(src) {
+    return src.match(/\|\|(?!\s)/)?.index;
+  },
+  tokenizer(src) {
+    const rule = /^\|\|(?!\s)([^\n]+)(?!\s)\|\|/;
+    const match = rule.exec(src);
+
+    if (match) {
+      console.log(src, match) // This shows me multiple matches.
+      return {
+        type: 'spoiler',
+        raw: match[0],
+        inner: this.lexer.inlineTokens(match[1].trim())
+      };
+    }
+  },
+  renderer(token) {
+    return `<span class="spoiler"><span>${this.parser.parseInline(token.inner)}</span></span>`;
+  }
+};
+
+marked.use({extensions: [spoilerExtension]})
+
+// console.log(marked.parseInline("Spoiler 测试： ||喵呜||喵呜"))
+
+export function mdParseInline(s: string) {return marked.parseInline(s)}
+export function mdParse(s: string) {return marked.parse(s)}
