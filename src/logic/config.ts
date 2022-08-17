05e83c0fab1cc1a2de4a9ef0a75c597ba1fd2240
Hykilpikonna <me@hydev.org>
2022-08-17 14:28:52 -0400

    [F] Fix debug host

--- src/logic/config.ts
+++ src/logic/config.ts
@@ -2,8 +2,8 @@ import urljoin from "url-join";
 
 export const backendHost = 'https://backend.one-among.us'
 // export const backendHost = 'http://localhost:43482'
-// export const dataHost = 'https://data.one-among.us'
-export const dataHost = 'http://localhost:8009'
+export const dataHost = 'https://data.one-among.us'
+// export const dataHost = 'http://localhost:8009'
 export const captchaSiteKey = '6LcbpzQdAAAAAN-J3dWZsi1t_ZRNT-ybUbmsQmH_'
 
 export function peopleUrl(id: string): string
