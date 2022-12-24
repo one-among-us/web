62849b9766360997ca38b67cac733efe27211901
Hykilpikonna <me@hydev.org>
2022-12-24 12:05:20 -0500

    [O] Autoimport

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import {createApp, h} from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'element-plus/dist/index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import Divider from "@/components/divider.vue";
 import RecaptchaV2 from "@/components/RecaptchaV2.vue"
 
-createApp(App).use(router).use(ElementPlus)
+createApp(App).use(router)
     .component('Divider', Divider)
     .component('RecaptchaV2', RecaptchaV2)
     .component('Dynamic', {
--- vite.config.ts
+++ vite.config.ts
@@ -5,38 +5,46 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import markdown from "./vite/markdown";
 
-// @ts-ignore
-import tsconfig from "./tsconfig.json";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-const tsconfigPathAliases = Object.fromEntries(
-  Object.entries(tsconfig.compilerOptions.paths).map(([key, values]) => {
-    let value = values[0];
-    if (key.endsWith("/*")) {
-      key = key.slice(0, -2);
-      value = value.slice(0, -2);
-    }
-
-    const nodeModulesPrefix = "node_modules/";
-    if (value.startsWith(nodeModulesPrefix)) {
-      value = value.replace(nodeModulesPrefix, "");
-    } else {
-      value = path.join(__dirname, value);
-    }
-
-    return [key, value];
-  })
-);
+const src = path.resolve(__dirname, 'src')
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    markdown()
+    markdown(),
+
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+
+      dts: path.resolve(src, 'auto-imports.d.ts'),
+    }),
+
+    Components({
+      resolvers: [
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+
+      dts: path.resolve(src, 'components.d.ts'),
+    }),
   ],
   resolve: {
     dedupe: ['vue'],
     alias: {
-      ...tsconfigPathAliases
+      '@': src
     }
   },
 
