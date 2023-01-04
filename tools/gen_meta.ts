cb549a0f4a64639aa3ec39b18a270b86c7a457ab
Azalea Gui <me@hydev.org>
2023-01-04 14:41:59 -0500

    [+] Create meta html function

--- tools/gen_meta.ts
+++ tools/gen_meta.ts
@@ -12,3 +12,30 @@ interface Meta {
   image?: string
   url?: string
 }
+
+function createMeta(meta: Meta): string
+{
+  const { title, desc, image, url } = meta
+  return `
+    <!-- Primary Meta Tags -->
+    <title>${title}</title>
+    <meta name="title" content="${title}">
+    <meta name="description" content="${desc}">
+  
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="${title}">
+    <meta property="og:description" content="${desc}">
+  
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:title" content="${title}">
+    <meta property="twitter:description" content="${desc}">
+  ` + (url ? `
+    <meta property="og:url" content="${url}">
+    <meta property="twitter:url" content="${url}">
+  ` : '') + (image? `
+    <meta property="og:image" content="${image}">
+    <meta property="twitter:image" content="${image}">
+  ` : '')
+}
