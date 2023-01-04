9b4e62fd42a021a4b05d196cc4d773a672cb667d
Azalea Gui <me@hydev.org>
2023-01-04 14:41:50 -0500

    [+] Meta model

--- tools/gen_meta.ts
+++ tools/gen_meta.ts
@@ -5,3 +5,10 @@ import fs from "fs-extra";
 const dist = "dist"
 const html = fs.readFileSync(path.join(dist, "index.html")).toString()
 
+
+interface Meta {
+  title: string
+  desc: string
+  image?: string
+  url?: string
+}
