3fe2913856b8371048ad596f8faf0fb31cfc6295
Azalea Gui <me@hydev.org>
2023-01-06 06:00:56 -0500

    [S] Style 404 page

--- src/views/FourOhFour.vue
+++ src/views/FourOhFour.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="four-oh-four unselectable">
+        <div class="top"></div>
         <div class="title">404</div>
         <div class="sub">这个页面不见了</div>
         <div class="click"><a href="javascript:history.back()">返回</a></div>
+        <div class="bottom"></div>
     </div>
 </template>
 
@@ -16,4 +18,32 @@ export default class FourOhFour extends Vue
 </script>
 
 <style lang="sass" scoped>
+@import "src/css/colors"
+
+.four-oh-four
+    display: flex
+    flex-direction: column
+    gap: 5px
+
+    .top
+        flex-grow: 1
+
+    .title
+        font-size: min(15em, 40vw)
+        color: transparent
+        background-color: rgb(188 140 68 / 50%)
+        -webkit-background-clip: text
+        text-shadow: rgba(255, 255, 255, 0.5) 0 5px 6px
+
+    .sub, .click
+        color: $color-text-light
+        font-size: 1.2em
+
+        a
+            color: $color-text-special
+            text-decoration: none
+
+    .bottom
+        flex-grow: 2
+
 </style>
