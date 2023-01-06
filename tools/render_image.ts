5da628ee2c8ee5ea66abb2bd5b74e129482e8119
Azalea Gui <me@hydev.org>
2023-01-06 02:27:21 -0500

    [U] Switch to serve-static

--- tools/render_image.ts
+++ tools/render_image.ts
@@ -1,16 +1,19 @@
 import puppeteer from 'puppeteer';
 import './extensions.js';
 
-import nodeStatic from 'node-static';
+import serveStatic from 'serve-static';
 import * as http from "http";
 import {createHttpTerminator} from "http-terminator";
+import fs from "fs-extra";
+
+import finalhandler from "finalhandler";
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 // Create HTTP server
-const file = new nodeStatic.Server('./dist');
-const server = http.createServer((request, response) => {
-  request.addListener('end', () => file.serve(request, response)).resume()
+const file = serveStatic('./dist');
+const server = http.createServer((req, res) => {
+  req.addListener('end', () => file(req, res, finalhandler(req, res))).resume()
 })
 server.listen(22745)
 const terminator = createHttpTerminator({ server })
