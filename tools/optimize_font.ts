aa75f08bde710df29b76cac6c681e91837d69f1b
Azalea Gui <me@hydev.org>
2023-01-09 15:18:30 -0500

    [+] Patch css in font optimization

--- tools/optimize_font.ts
+++ tools/optimize_font.ts
@@ -11,28 +11,22 @@ export async function optimize_font(file: string, characters: string, dist: stri
   for (const ext of exts)
   {
     console.log("> Converting to", ext)
+    const orig = file.with_ext(ext)
     const nf = file.with_ext(`optimized.${ext}`)
     fs.writeFileSync(nf, await subsetFont(buf, characters, {targetFormat: ext}))
 
     // Replace CSS font names
-    console.log('> Patched:', await replaceInFiles({
-      files: dist + '/*',
-      from: file.with_ext(ext).file_name(),
+    console.log(`> Replacing ${orig.file_name()} with ${nf.file_name()}`)
+    console.log('> Patched:', (await replaceInFiles({
+      files: dist + '/**/*.css',
+      from: orig.file_name(),
       to: nf.file_name()
-    }))
+    })).paths)
   }
 
   // Delete old fonts
+  console.log("> Deleting old fonts...")
   fs.removeSync(file)
   for (const ext of ['woff2', 'woff'])
     fs.removeSync(file.with_ext(ext))
-
-  // Replace CSS font names
-  for (const ext of exts)
-  {
-    const orig = file.with_ext(ext)
-    const new_ = file.with_ext(`optimized.${ext}`)
-
-
-  }
 }
