f61e5d453e9c9cc7c5406e79a897e11787f61d33
Azalea Gui <me@hydev.org>
2023-01-09 00:55:31 -0500

    [O] Conditionally import element css
    index.css 422.27 KB > 112.64 KB

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import router from './router'
 import '@fortawesome/fontawesome-free/css/solid.min.css'
 import '@fortawesome/fontawesome-free/css/brands.min.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
-import 'element-plus/dist/index.css'
+
+// Import only the css necessary instead of importing everything:
+// import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/theme-chalk/el-message-box.css'
+import 'element-plus/theme-chalk/el-overlay.css'
+
 import Divider from "@/components/divider.vue";
 import RecaptchaV2 from "@/components/RecaptchaV2.vue"
 
