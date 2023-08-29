8b25cee965f2dceabf50d0f67f8f1ba6af522717
BeiyanYunyi <BeiyanYunyi@users.noreply.github.com>
2023-08-29 15:38:52 +0000

    Deploying to gh-pages from @ one-among-us/web@bc636d05681177012b3313863cc143116478855c 🚀

--- assets/Ref-c1028ef1.js
+++
@@ -1,1 +0,0 @@
-import{bU as c}from"./index-e1720cfd.js";function u(t){return c((e,r)=>{e.computed||(e.computed=Object.create(null)),e.computed[r]={cache:!1,get(){return this.$refs[t||r]}}})}export{u as R};
