78dfb53cffa6bc905cab347df2f2383dd82d8fe6
hykilpikonna <hykilpikonna@users.noreply.github.com>
2023-11-27 07:17:55 +0000

    Deploying to gh-pages from @ one-among-us/web@302900464d9c4b5e945c064d2a0c6336b6f11c9d 🚀

---
+++ assets/About-55c15def.js
@@ -0,0 +1,22 @@
+var x=Object.defineProperty;var u=(n,t,e)=>t in n?x(n,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):n[t]=e;var r=(n,t,e)=>(u(n,typeof t!="symbol"?t+"":t,e),e);import{V as c,g as h,O as m,_ as p,o as g,c as f,a as d}from"./index-da207e33.js";const _=`<h1 id="联系我们">联系我们</h1>
+<p>这个网站目前由一个非营利组织的志愿者团队维护。源代码共享于 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 组织页面</a>。</p>
+<ul>
+<li>你可以发送邮件到 <strong><a href="mailto:&#x69;&#x6e;&#102;&#x6f;&#x40;&#111;&#110;&#x65;&#45;&#97;&#109;&#111;&#110;&#x67;&#x2e;&#117;&#x73;">&#x69;&#x6e;&#102;&#x6f;&#x40;&#111;&#110;&#x65;&#45;&#97;&#109;&#111;&#110;&#x67;&#x2e;&#117;&#x73;</a></strong>，贡献内容或者提出批评建议。</li>
+<li>你也可以直接通过 GitHub 的 issues 与 pull requests 功能与我们联系。</li>
+<li>我们的 <a href="https://twitter.com/oneamong_us">推特账号</a> 欢迎私信以及各种形式的留言。</li>
+</ul>
+`,b=`<h1 id="聯繫我們">聯繫我們</h1>
+<p>這個網站目前由一個非營利組織的誌願者團隊維護，原始碼共享於 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 組織頁面</a>。</p>
+<ul>
+<li>你可以發送 E-mail 到 <strong><a href="mailto:&#x69;&#x6e;&#x66;&#111;&#64;&#111;&#110;&#x65;&#x2d;&#x61;&#x6d;&#x6f;&#x6e;&#x67;&#x2e;&#x75;&#x73;">&#x69;&#x6e;&#x66;&#111;&#64;&#111;&#110;&#x65;&#x2d;&#x61;&#x6d;&#x6f;&#x6e;&#x67;&#x2e;&#x75;&#x73;</a></strong>，貢獻內容或者提出批評建議。</li>
+<li>你也可以直接通過 GitHub 的 issues 與 pull requests 功能與我們聯系。</li>
+<li>我們的 <a href="https://twitter.com/oneamong_us">推特賬戶</a> 歡迎私訊以及各種形式的留言。</li>
+</ul>
+`,H=`<h1 id="contact-us">Contact US</h1>
+<p>This site is currently maintained by a team of volunteers of a non-profit organization. Source code is on <a href="https://github.com/one-among-us">One Among Us GitHub organization page</a>.  </p>
+<ul>
+<li>You can send email to <strong><a href="mailto:&#105;&#110;&#102;&#111;&#64;&#x6f;&#110;&#x65;&#45;&#97;&#109;&#x6f;&#x6e;&#x67;&#x2e;&#117;&#115;">&#105;&#110;&#102;&#111;&#64;&#x6f;&#110;&#x65;&#45;&#97;&#109;&#x6f;&#x6e;&#x67;&#x2e;&#117;&#115;</a></strong> to contribute or put forward criticism and suggestions.</li>
+<li>You can also contact us through opening a GitHub issue or Pull request.</li>
+<li>Our <a href="https://twitter.com/oneamong_us">Twitter account</a> welcomes private messages and comments.</li>
+</ul>
+`;var v=Object.defineProperty,w=Object.getOwnPropertyDescriptor,O=(n,t,e,s)=>{for(var o=s>1?void 0:s?w(t,e):t,a=n.length-1,i;a>=0;a--)(i=n[a])&&(o=(s?i(t,e,o):i(o))||o);return s&&o&&v(t,e,o),o};let l=class extends c{constructor(){super(...arguments);r(this,"lang",h());r(this,"html",this.lang==="zh_hans"?_:this.lang==="zh_hant"?b:H)}};l=O([m({})],l);const A={id:"About"},G=["innerHTML"];function P(n,t,e,s,o,a){return g(),f("div",A,[d("div",{class:"markdown-content",style:{margin:"10px min(4vw, 40px)"},innerHTML:n.html},null,8,G)])}const U=p(l,[["render",P]]);export{U as default};
