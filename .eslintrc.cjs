c84ff1cfc2ec8b08a6a960a1a8a51fac5b3fdf03
Azalea Gui <me@hydev.org>
2024-01-14 11:23:12 -0500

    [F] Fix captcha not hiding after completion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "@typescript-eslint/ban-ts-comment": "off",
-    'vue/require-v-for-key': 'warn'
+    'vue/require-v-for-key': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
   }
 }
