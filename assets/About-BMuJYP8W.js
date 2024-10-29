eedf9e9108b56b6ea8428a2ce6a2a7b95a30a601
LS-KR <LS-KR@users.noreply.github.com>
2024-10-29 06:46:51 +0000

    Deploying to gh-pages from @ one-among-us/web@357dd28af0f464422b1a441967e900bda83cc904 🚀

---
+++ assets/About-BMuJYP8W.js
@@ -0,0 +1,22 @@
+var x=Object.defineProperty;var u=(t,n,e)=>n in t?x(t,n,{enumerable:!0,configurable:!0,writable:!0,value:e}):t[n]=e;var s=(t,n,e)=>(u(t,typeof n!="symbol"?n+"":n,e),e);import{V as c,a6 as h,a7 as m,C as f,_ as g,c as p,a as d,o as b}from"./index-CGEIFIeY.js";const _=`<h1 id="contact-us">Contact US</h1>
+<p>This site is currently maintained by a team of volunteers of a non-profit organization. Source code is on <a href="https://github.com/one-among-us">One Among Us GitHub organization page</a>.  </p>
+<ul>
+<li>You can send email to <strong><a href="mailto:&#x69;&#110;&#x66;&#x6f;&#x40;&#x6f;&#x6e;&#101;&#x2d;&#x61;&#109;&#x6f;&#x6e;&#x67;&#46;&#117;&#x73;">&#x69;&#110;&#x66;&#x6f;&#x40;&#x6f;&#x6e;&#101;&#x2d;&#x61;&#109;&#x6f;&#x6e;&#x67;&#46;&#117;&#x73;</a></strong> to contribute or put forward criticism and suggestions.</li>
+<li>You can also contact us through opening a GitHub issue or Pull request.</li>
+<li>Our <a href="https://twitter.com/oneamong_us">Twitter account</a> welcomes private messages and comments.</li>
+</ul>
+`,H=`<h1 id="联系我们">联系我们</h1>
+<p>这个网站目前由一个非营利组织的志愿者团队维护。源代码共享于 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 组织页面</a>。</p>
+<ul>
+<li>你可以发送邮件到 <strong><a href="mailto:&#x69;&#110;&#x66;&#x6f;&#64;&#x6f;&#x6e;&#101;&#45;&#97;&#x6d;&#x6f;&#110;&#103;&#x2e;&#117;&#x73;">&#x69;&#110;&#x66;&#x6f;&#64;&#x6f;&#x6e;&#101;&#45;&#97;&#x6d;&#x6f;&#110;&#103;&#x2e;&#117;&#x73;</a></strong>，贡献内容或者提出批评建议。</li>
+<li>你也可以直接通过 GitHub 的 issues 与 pull requests 功能与我们联系。</li>
+<li>我们的 <a href="https://twitter.com/oneamong_us">推特账号</a> 欢迎私信以及各种形式的留言。</li>
+</ul>
+`,v=`<h1 id="聯繫我們">聯繫我們</h1>
+<p>這個網站目前由一個非營利組織的誌願者團隊維護，原始碼共享於 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 組織頁面</a>。</p>
+<ul>
+<li>你可以發送 E-mail 到 <strong><a href="mailto:&#x69;&#110;&#102;&#x6f;&#64;&#111;&#110;&#x65;&#x2d;&#97;&#109;&#x6f;&#110;&#103;&#x2e;&#x75;&#115;">&#x69;&#110;&#102;&#x6f;&#64;&#111;&#110;&#x65;&#x2d;&#97;&#109;&#x6f;&#110;&#103;&#x2e;&#x75;&#115;</a></strong>，貢獻內容或者提出批評建議。</li>
+<li>你也可以直接通過 GitHub 的 issues 與 pull requests 功能與我們聯系。</li>
+<li>我們的 <a href="https://twitter.com/oneamong_us">推特賬戶</a> 歡迎私訊以及各種形式的留言。</li>
+</ul>
+`;var w=(t,n,e,r)=>{for(var o=n,a=t.length-1,l;a>=0;a--)(l=t[a])&&(o=l(o)||o);return o};let i=class extends c{constructor(){super(...arguments);s(this,"lang",h());s(this,"html",m(this.lang==="zh_hans"?H:this.lang==="zh_hant"?v:_))}};i=w([f({})],i);const A={id:"About"},G=["innerHTML"];function k(t,n,e,r,o,a){return b(),p("div",A,[d("div",{class:"markdown-content",style:{margin:"10px min(4vw, 40px)"},innerHTML:t.html},null,8,G)])}const C=g(i,[["render",k]]);export{C as default};
