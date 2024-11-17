d911d7310ed2ae7d42dac0029a66725485d0788d
LS-KR <LS-KR@users.noreply.github.com>
2024-11-17 16:33:32 +0000

    Deploying to gh-pages from @ one-among-us/web@cd8fbce2a13236dbb0988a2b4fe7a900c6ad2bf2 🚀

--- assets/About-C8LZR0h6.js
+++ assets/About-C8LZR0h6.js
@@ -1,21 +1,21 @@
-var x=Object.defineProperty;var u=(t,n,e)=>n in t?x(t,n,{enumerable:!0,configurable:!0,writable:!0,value:e}):t[n]=e;var s=(t,n,e)=>(u(t,typeof n!="symbol"?n+"":n,e),e);import{V as c,a6 as h,a7 as m,C as g,_ as f,c as p,a as d,o as b}from"./index-BaCEAzob.js";const _=`<h1 id="contact-us">Contact US</h1>
+var u=Object.defineProperty;var x=(t,n,e)=>n in t?u(t,n,{enumerable:!0,configurable:!0,writable:!0,value:e}):t[n]=e;var s=(t,n,e)=>(x(t,typeof n!="symbol"?n+"":n,e),e);import{V as c,a6 as h,a7 as m,C as g,_ as f,c as p,a as d,o as b}from"./index-B-G1FQCd.js";const _=`<h1 id="contact-us">Contact US</h1>
 <p>This site is currently maintained by a team of volunteers of a non-profit organization. Source code is on <a href="https://github.com/one-among-us">One Among Us GitHub organization page</a>.  </p>
 <ul>
-<li>You can send email to <strong><a href="mailto:&#x69;&#x6e;&#102;&#x6f;&#x40;&#111;&#x6e;&#101;&#45;&#97;&#x6d;&#111;&#x6e;&#x67;&#x2e;&#x75;&#115;">&#x69;&#x6e;&#102;&#x6f;&#x40;&#111;&#x6e;&#101;&#45;&#97;&#x6d;&#111;&#x6e;&#x67;&#x2e;&#x75;&#115;</a></strong> to contribute or put forward criticism and suggestions.</li>
+<li>You can send email to <strong><a href="mailto:&#x69;&#x6e;&#102;&#x6f;&#64;&#x6f;&#110;&#101;&#x2d;&#97;&#x6d;&#x6f;&#110;&#x67;&#46;&#x75;&#115;">&#x69;&#x6e;&#102;&#x6f;&#64;&#x6f;&#110;&#101;&#x2d;&#97;&#x6d;&#x6f;&#110;&#x67;&#46;&#x75;&#115;</a></strong> to contribute or put forward criticism and suggestions.</li>
 <li>You can also contact us through opening a GitHub issue or Pull request.</li>
 <li>Our <a href="https://twitter.com/oneamong_us">Twitter account</a> welcomes private messages and comments.</li>
 </ul>
 `,H=`<h1 id="联系我们">联系我们</h1>
 <p>这个网站目前由一个非营利组织的志愿者团队维护。源代码共享于 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 组织页面</a>。</p>
 <ul>
-<li>你可以发送邮件到 <strong><a href="mailto:&#x69;&#110;&#102;&#x6f;&#64;&#x6f;&#x6e;&#x65;&#45;&#x61;&#109;&#x6f;&#x6e;&#x67;&#x2e;&#117;&#x73;">&#x69;&#110;&#102;&#x6f;&#64;&#x6f;&#x6e;&#x65;&#45;&#x61;&#109;&#x6f;&#x6e;&#x67;&#x2e;&#117;&#x73;</a></strong>，贡献内容或者提出批评建议。</li>
+<li>你可以发送邮件到 <strong><a href="mailto:&#105;&#110;&#x66;&#x6f;&#64;&#111;&#110;&#x65;&#45;&#x61;&#x6d;&#111;&#x6e;&#x67;&#46;&#117;&#115;">&#105;&#110;&#x66;&#x6f;&#64;&#111;&#110;&#x65;&#45;&#x61;&#x6d;&#111;&#x6e;&#x67;&#46;&#117;&#115;</a></strong>，贡献内容或者提出批评建议。</li>
 <li>你也可以直接通过 GitHub 的 issues 与 pull requests 功能与我们联系。</li>
 <li>我们的 <a href="https://twitter.com/oneamong_us">推特账号</a> 欢迎私信以及各种形式的留言。</li>
 </ul>
 `,v=`<h1 id="聯繫我們">聯繫我們</h1>
 <p>這個網站目前由一個非營利組織的誌願者團隊維護，原始碼共享於 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 組織頁面</a>。</p>
 <ul>
-<li>你可以發送 E-mail 到 <strong><a href="mailto:&#x69;&#110;&#102;&#111;&#x40;&#111;&#110;&#x65;&#x2d;&#x61;&#109;&#x6f;&#110;&#x67;&#46;&#117;&#115;">&#x69;&#110;&#102;&#111;&#x40;&#111;&#110;&#x65;&#x2d;&#x61;&#109;&#x6f;&#110;&#x67;&#46;&#117;&#115;</a></strong>，貢獻內容或者提出批評建議。</li>
+<li>你可以發送 E-mail 到 <strong><a href="mailto:&#105;&#x6e;&#102;&#111;&#x40;&#x6f;&#110;&#101;&#45;&#97;&#109;&#x6f;&#110;&#x67;&#x2e;&#117;&#x73;">&#105;&#x6e;&#102;&#111;&#x40;&#x6f;&#110;&#101;&#45;&#97;&#109;&#x6f;&#110;&#x67;&#x2e;&#117;&#x73;</a></strong>，貢獻內容或者提出批評建議。</li>
 <li>你也可以直接通過 GitHub 的 issues 與 pull requests 功能與我們聯系。</li>
 <li>我們的 <a href="https://twitter.com/oneamong_us">推特賬戶</a> 歡迎私訊以及各種形式的留言。</li>
 </ul>
