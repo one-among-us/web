b6b2160640f9b33ae936f6c637d06c39640c4b83
LS-KR <LS-KR@users.noreply.github.com>
2024-12-21 05:53:50 +0000

    Deploying to gh-pages from @ one-among-us/web@f9122beaf8485b5efd2fc4b410dab4d3330b67ca 🚀

---
+++ assets/About-B-jS1aCM.js
@@ -0,0 +1,22 @@
+var l=Object.defineProperty;var m=(o,n,t)=>n in o?l(o,n,{enumerable:!0,configurable:!0,writable:!0,value:t}):o[n]=t;var r=(o,n,t)=>m(o,typeof n!="symbol"?n+"":n,t);import{V as c,a6 as h,a7 as g,C as p,_ as f,c as _,a as b,o as d}from"./index-DFrBLVoA.js";const H=`<h1>Contact US</h1>
+<p>This site is currently maintained by a team of volunteers of a non-profit organization. Source code is on <a href="https://github.com/one-among-us">One Among Us GitHub organization page</a>.  </p>
+<ul>
+<li>You can send email to <strong><a href="mailto:info@one-among.us">info@one-among.us</a></strong> to contribute or put forward criticism and suggestions.</li>
+<li>You can also contact us through opening a GitHub issue or Pull request.</li>
+<li>Our <a href="https://twitter.com/oneamong_us">Twitter account</a> welcomes private messages and comments.</li>
+</ul>
+`,v=`<h1>联系我们</h1>
+<p>这个网站目前由一个非营利组织的志愿者团队维护。源代码共享于 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 组织页面</a>。</p>
+<ul>
+<li>你可以发送邮件到 <strong><a href="mailto:info@one-among.us">info@one-among.us</a></strong>，贡献内容或者提出批评建议。</li>
+<li>你也可以直接通过 GitHub 的 issues 与 pull requests 功能与我们联系。</li>
+<li>我们的 <a href="https://twitter.com/oneamong_us">推特账号</a> 欢迎私信以及各种形式的留言。</li>
+</ul>
+`,w=`<h1>聯繫我們</h1>
+<p>這個網站目前由一個非營利組織的誌願者團隊維護，原始碼共享於 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 組織頁面</a>。</p>
+<ul>
+<li>你可以發送 E-mail 到 <strong><a href="mailto:info@one-among.us">info@one-among.us</a></strong>，貢獻內容或者提出批評建議。</li>
+<li>你也可以直接通過 GitHub 的 issues 與 pull requests 功能與我們聯系。</li>
+<li>我們的 <a href="https://twitter.com/oneamong_us">推特賬戶</a> 歡迎私訊以及各種形式的留言。</li>
+</ul>
+`;var O=Object.defineProperty,A=Object.getOwnPropertyDescriptor,G=(o,n,t,s)=>{for(var e=s>1?void 0:s?A(n,t):n,a=o.length-1,i;a>=0;a--)(i=o[a])&&(e=(s?i(n,t,e):i(e))||e);return s&&e&&O(n,t,e),e};let u=class extends c{constructor(){super(...arguments);r(this,"lang",h());r(this,"html",g(this.lang==="zh_hans"?v:this.lang==="zh_hant"?w:H))}};u=G([p({})],u);const x={id:"About"},P=["innerHTML"];function $(o,n,t,s,e,a){return d(),_("div",x,[b("div",{class:"markdown-content",style:{margin:"10px min(4vw, 40px)"},innerHTML:o.html},null,8,P)])}const U=f(u,[["render",$]]);export{U as default};
