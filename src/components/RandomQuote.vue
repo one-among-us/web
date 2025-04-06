84183fdffc3d596dfd9b545cba983f8fb2098084
Harmoni Karman <73192825+DSLapatura@users.noreply.github.com>
2025-04-06 12:54:30 -0700

    [+] RandomQuote
    (First time writing Vue sorry about the poor quality)

---
+++ src/components/RandomQuote.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-facing-decorator';
+
+@Component({})
+export default class RandomQuote extends Vue {
+    @Prop({ required: true }) messages: string[]
+}
+</script>
+
+<template>
+    <p v-html="this.messages[Math.floor(this.messages.length * Math.random())]"> </p>
+</template>
