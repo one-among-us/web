dc6d7b36fbc03fb8de9e3601eb1f77169f869989
Elihuso Quigley <Elihuso@outlook.com>
2025-04-07 08:27:40 +0800

    [O] style && roll

--- src/components/RandomSpan.vue
+++ src/components/RandomSpan.vue
@@ -1,12 +1,33 @@
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-facing-decorator';
+import {randint} from "@/logic/helper";
 
 @Component({})
 export default class RandomQuote extends Vue {
     @Prop({ required: true }) messages: string[]
+
+    m = "";
+
+    created() {
+        this.roll();
+    }
+
+    roll() {
+        this.m = this.messages[randint(0, this.messages.length - 1)];
+    }
 }
 </script>
 
 <template>
-    <p v-html="this.messages[Math.floor(this.messages.length * Math.random())]"> </p>
+    <span class="markdown-content random-span" v-text="m" @click="roll()"></span>
 </template>
+
+<style lang="scss">
+@import "@/css/markdown";
+
+.random-span {
+    display: inline;
+    cursor: pointer;
+    user-select: none;
+}
+</style>
