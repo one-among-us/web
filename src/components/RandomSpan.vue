f015e208112f9f29020fddc250b78aa8cd6755e5
Harmoni Karman <73192825+DSLapatura@users.noreply.github.com>
2025-04-06 20:59:02 -0700

    [O] html support and more
    HTML support, CanRoll, Bugfix, etc.

--- src/components/RandomSpan.vue
+++ src/components/RandomSpan.vue
@@ -1,17 +1,20 @@
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-facing-decorator';
 import {randint} from "@/logic/helper";
-
 @Component({})
-export default class RandomQuote extends Vue {
+export default class RandomSpan extends Vue {
     @Prop({ required: true }) messages: string[]
-
+    @Prop canRoll: boolean
     m = "";
-
     created() {
         this.roll();
     }
-
+    tryRoll() {
+        if (this.canRoll) 
+        {
+            this.roll()
+        }
+    }
     roll() {
         this.m = this.messages[randint(0, this.messages.length - 1)];
     }
@@ -19,12 +22,11 @@ export default class RandomQuote extends Vue {
 </script>
 
 <template>
-    <span class="markdown-content random-span" v-text="m" @click="roll()"></span>
+    <span class="markdown-content random-span" v-html="m" @click="tryRoll()"></span>
 </template>
 
 <style lang="scss">
 @import "@/css/markdown";
-
 .random-span {
     display: inline;
     cursor: pointer;
