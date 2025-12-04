6f3cecf9e1d1b9c6974278b7a78788946a1f2931
Misaka13514 <Misaka13514@users.noreply.github.com>
2025-12-04 09:14:00 +0000

    Deploying to gh-pages from @ one-among-us/web@2ffcc23bae55a77c96ec7796a81b7bcebc4f6dfc 🚀

---
+++ assets/About-C-Rsttlg.js
@@ -0,0 +1,25 @@
+var u=Object.defineProperty;var m=(n,t,a)=>t in n?u(n,t,{enumerable:!0,configurable:!0,writable:!0,value:a}):n[t]=a;var i=(n,t,a)=>m(n,typeof t!="symbol"?t+"":t,a);import{V as h,a6 as x,a7 as c,C as g,_ as p,c as f,a as b,o as d}from"./index-CfTJRNQK.js";const O=`<h1 id="contact-us">Contact US</h1>
+<p>This site is currently maintained by a team of volunteers of a non-profit organization. Source code is on <a href="https://github.com/one-among-us">One Among Us GitHub organization page</a>.  </p>
+<ul>
+<li>You can send email to <strong><a href="mailto:&#105;&#110;&#102;&#x6f;&#x40;&#x6f;&#x6e;&#101;&#45;&#97;&#109;&#x6f;&#x6e;&#103;&#x2e;&#x75;&#115;">&#105;&#110;&#102;&#x6f;&#x40;&#x6f;&#x6e;&#101;&#45;&#97;&#109;&#x6f;&#x6e;&#103;&#x2e;&#x75;&#115;</a></strong> to contribute or put forward criticism and suggestions.</li>
+<li>You can also contact us through opening a GitHub issue or Pull request.</li>
+<li>Our <a href="https://twitter.com/oneamong_us">Twitter account</a> welcomes private messages and comments.</li>
+<li>If you want to contribute to our repo, please check our <a href="https://github.com/one-among-us/data/blob/main/CODE_OF_CONDUCT.md">CODE_OF_CONDUCT</a> and <a href="https://github.com/one-among-us/data/blob/main/CONTRIBUTING.md">CONTRIBUTING</a>.</li>
+</ul>
+`,_=`<h1 id="联系我们">联系我们</h1>
+<p>这个网站目前由一个非营利组织的志愿者团队维护。源代码共享于 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 组织页面</a>。</p>
+<ul>
+<li>你可以发送邮件到 <strong><a href="mailto:&#x69;&#x6e;&#x66;&#x6f;&#64;&#x6f;&#x6e;&#101;&#45;&#x61;&#109;&#111;&#x6e;&#103;&#46;&#x75;&#115;">&#x69;&#x6e;&#x66;&#x6f;&#64;&#x6f;&#x6e;&#101;&#45;&#x61;&#109;&#111;&#x6e;&#103;&#46;&#x75;&#115;</a></strong>，贡献内容或者提出批评建议。</li>
+<li>你也可以直接通过 GitHub 的 issues 与 pull requests 功能与我们联系。</li>
+<li>我们的 <a href="https://twitter.com/oneamong_us">推特账号</a> 欢迎私信以及各种形式的留言。</li>
+<li>如果想为我们的 repo 做出贡献，请查看 <a href="https://github.com/one-among-us/data/blob/main/CODE_OF_CONDUCT.md">CODE_OF_CONDUCT</a> 和 <a href="https://github.com/one-among-us/data/blob/main/CONTRIBUTING.md">CONTRIBUTING</a>。</li>
+</ul>
+`,C=`<h1 id="聯繫我們">聯繫我們</h1>
+<p>這個網站目前由一個非營利組織的誌願者團隊維護，原始碼共享於 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 組織頁面</a>。</p>
+<ul>
+<li>你可以發送 E-mail 到 <strong><a href="mailto:&#x69;&#110;&#102;&#x6f;&#x40;&#111;&#x6e;&#101;&#45;&#x61;&#109;&#x6f;&#x6e;&#103;&#x2e;&#x75;&#115;">&#x69;&#110;&#102;&#x6f;&#x40;&#111;&#x6e;&#101;&#45;&#x61;&#109;&#x6f;&#x6e;&#103;&#x2e;&#x75;&#115;</a></strong>，貢獻內容或者提出批評建議。</li>
+<li>你也可以直接通過 GitHub 的 issues 與 pull requests 功能與我們聯系。</li>
+<li>我們的 <a href="https://twitter.com/oneamong_us">推特賬戶</a> 歡迎私訊以及各種形式的留言。</li>
+<li>如果想為我們的 repo 做出貢獻，請查看我們的 <a href="https://github.com/one-among-us/data/blob/main/CODE_OF_CONDUCT.md">CODE_OF_CONDUCT</a> 和 <a href="https://github.com/one-among-us/data/blob/main/CONTRIBUTING.md">CONTRIBUTING</a>。</li>
+</ul>
+`;var T=Object.getOwnPropertyDescriptor,N=(n,t,a,s)=>{for(var o=s>1?void 0:s?T(t,a):t,e=n.length-1,l;e>=0;e--)(l=n[e])&&(o=l(o)||o);return o};let r=class extends h{constructor(){super(...arguments);i(this,"lang",x());i(this,"html",c(this.lang==="zh_hans"?_:this.lang==="zh_hant"?C:O))}};r=N([g({})],r);const U={id:"About"},D=["innerHTML"];function I(n,t,a,s,o,e){return d(),f("main",U,[b("div",{class:"markdown-content",style:{margin:"10px min(4vw, 40px)"},innerHTML:n.html},null,8,D)])}const v=p(r,[["render",I]]);export{v as default};
