20d5b75a3bad2fae2f15a653b61d624062b642ee
hykilpikonna <hykilpikonna@users.noreply.github.com>
2026-04-23 20:34:41 +0000

    Deploying to gh-pages from @ one-among-us/web@23f3c55054aae1735b638823801b22387fb95bfa 🚀

---
+++ assets/About-B05fxlm3.js
@@ -0,0 +1,25 @@
+import{d as o,X as a,Y as e,c as i,b as s,u,o as m}from"./index-wOY2Z9UA.js";const r=`<h1 id="contact-us">Contact US</h1>
+<p>This site is currently maintained by a team of volunteers of a non-profit organization. Source code is on <a href="https://github.com/one-among-us">One Among Us GitHub organization page</a>.  </p>
+<ul>
+<li>You can send email to <strong><a href="mailto:&#x69;&#x6e;&#x66;&#x6f;&#64;&#111;&#110;&#x65;&#45;&#97;&#x6d;&#x6f;&#x6e;&#103;&#46;&#117;&#x73;">&#x69;&#x6e;&#x66;&#x6f;&#64;&#111;&#110;&#x65;&#45;&#97;&#x6d;&#x6f;&#x6e;&#103;&#46;&#117;&#x73;</a></strong> to contribute or put forward criticism and suggestions.</li>
+<li>You can also contact us through opening a GitHub issue or Pull request.</li>
+<li>Our <a href="https://twitter.com/oneamong_us">Twitter account</a> welcomes private messages and comments.</li>
+<li>If you want to contribute to our repo, please check our <a href="https://github.com/one-among-us/data/blob/main/CODE_OF_CONDUCT.md">CODE_OF_CONDUCT</a> and <a href="https://github.com/one-among-us/data/blob/main/CONTRIBUTING.md">CONTRIBUTING</a>.</li>
+</ul>
+`,l=`<h1 id="联系我们">联系我们</h1>
+<p>这个网站目前由一个非营利组织的志愿者团队维护。源代码共享于 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 组织页面</a>。</p>
+<ul>
+<li>你可以发送邮件到 <strong><a href="mailto:&#105;&#110;&#x66;&#x6f;&#64;&#111;&#x6e;&#101;&#45;&#97;&#109;&#x6f;&#x6e;&#x67;&#x2e;&#117;&#x73;">&#105;&#110;&#x66;&#x6f;&#64;&#111;&#x6e;&#101;&#45;&#97;&#109;&#x6f;&#x6e;&#x67;&#x2e;&#117;&#x73;</a></strong>，贡献内容或者提出批评建议。</li>
+<li>你也可以直接通过 GitHub 的 issues 与 pull requests 功能与我们联系。</li>
+<li>我们的 <a href="https://twitter.com/oneamong_us">推特账号</a> 欢迎私信以及各种形式的留言。</li>
+<li>如果想为我们的 repo 做出贡献，请查看 <a href="https://github.com/one-among-us/data/blob/main/CODE_OF_CONDUCT.md">CODE_OF_CONDUCT</a> 和 <a href="https://github.com/one-among-us/data/blob/main/CONTRIBUTING.md">CONTRIBUTING</a>。</li>
+</ul>
+`,h=`<h1 id="聯繫我們">聯繫我們</h1>
+<p>這個網站目前由一個非營利組織的誌願者團隊維護，原始碼共享於 <a href="https://github.com/one-among-us">One Among Us 的 GitHub 組織頁面</a>。</p>
+<ul>
+<li>你可以發送 E-mail 到 <strong><a href="mailto:&#x69;&#x6e;&#x66;&#111;&#64;&#111;&#110;&#101;&#45;&#97;&#109;&#x6f;&#110;&#x67;&#46;&#x75;&#115;">&#x69;&#x6e;&#x66;&#111;&#64;&#111;&#110;&#101;&#45;&#97;&#109;&#x6f;&#110;&#x67;&#46;&#x75;&#115;</a></strong>，貢獻內容或者提出批評建議。</li>
+<li>你也可以直接通過 GitHub 的 issues 與 pull requests 功能與我們聯系。</li>
+<li>我們的 <a href="https://twitter.com/oneamong_us">推特賬戶</a> 歡迎私訊以及各種形式的留言。</li>
+<li>如果想為我們的 repo 做出貢獻，請查看我們的 <a href="https://github.com/one-among-us/data/blob/main/CODE_OF_CONDUCT.md">CODE_OF_CONDUCT</a> 和 <a href="https://github.com/one-among-us/data/blob/main/CONTRIBUTING.md">CONTRIBUTING</a>。</li>
+</ul>
+`,c={id:"About"},x=["innerHTML"],d=o({name:"AboutView",__name:"About",setup(g){const t=a(),n=e(t==="zh_hans"?l:t==="zh_hant"?h:r);return(p,b)=>(m(),i("main",c,[s("div",{class:"markdown-content",style:{margin:"10px min(4vw, 40px)"},innerHTML:u(n)},null,8,x)]))}});export{d as default};
